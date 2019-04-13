f08b30799e1dcadea6892ea42368bc02c4e5c513
ravenusmc <mcuddy77@gmail.com>
2019-04-13 16:12:54 -0400

    built initial csv file for porject

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -19,28 +19,33 @@ class Scraping():
         self.soup = BeautifulSoup(self.response.text, "html.parser")
 
 
-    def push_into_csv(self):
+    def build_csv_file(self):
         #opening up the csv file that I'll write to
         with open("battle_data.csv", "w") as csv_file:
             #Creating a csv variable
             csv_writer = writer(csv_file)
             #This line will write the rows to the CSV file, the header rows.
-            csv_writer.writerow(["Year", "Name", "Location", "Belligerents", "Result"])
+            csv_writer.writerow(["Year", "Name", "Location", "Belligerent_1", "Belligerent_2", "Result"])
             #Getting the table from the wikipedia page
             table = self.soup.find('table', {'class':'wikitable'})
-            row_marker = 0
             #Looping through each row to get all of the data
             for row in table.find_all('tr'):
-                column_marker = 0
+                #grabbing each row of data
                 columns = row.find_all('td')
-                print(columns)
-                input()
+                #This list will hold each rows data points which will be used to
+                #build the csv
+                data_list = []
+                for column in columns:
+                    #Getting the actual text out of the html
+                    column_data = column.get_text().replace('\n', '')
+                    #adding each column to the list.
+                    data_list.append(column_data)
+                #This will ensure that only list that have data are inserted
+                #into the csv file.
+                if data_list:
+                    #Adding each list that's built to the csv
+                    csv_writer.writerow(data_list)
 
-                #csv_writer.writerow([year, total, black, jewish, islamic])
-                # for column in columns:
-                #     new_table.iat[row_marker,column_marker] = column.get_text()
-                #     column_marker += 1
-
-scrape = Scraping()
-scrape.setup_soup()
-scrape.push_into_csv()
+# scrape = Scraping()
+# scrape.setup_soup()
+# scrape.build_csv_file()
