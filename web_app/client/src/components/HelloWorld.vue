24b1c27d162cb131c1c3e27a890f5ce5d4960885
ravenusmc <mcuddy77@gmail.com>
2019-04-13 16:23:08 -0400

    added vue to project and set up basic file for python server

---
+++ web_app/client/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hi</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
