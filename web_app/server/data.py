c6877acc886f3717af10c6e1ea791aef5f7ba273
ravenusmc <mcuddy77@gmail.com>
2019-04-23 20:06:58 -0400

    added file to start working with data

---
+++ web_app/server/data.py
@@ -0,0 +1,17 @@
+#This file will contain the methods and class that will deal with all the data
+
+#importing supporting libraries
+import numpy as np
+import pandas as pd
+
+class Data():
+
+    def __init__(self):
+        self.data = pd.read_csv('./data/battle_data.csv')
+
+    def basic_info(self):
+        print(self.data.head())
+        print(self.data.dtypes)
+
+obj = Data()
+obj.basic_info()
