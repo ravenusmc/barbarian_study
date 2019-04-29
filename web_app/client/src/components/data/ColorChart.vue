feb849dabdb144fb6392dcd9de56e8761e756edf
ravenusmc <mcuddy77@gmail.com>
2019-04-28 20:02:26 -0400

    working on form for color chart

--- web_app/client/src/components/data/ColorChart.vue
+++ web_app/client/src/components/data/ColorChart.vue
@@ -9,7 +9,13 @@
       <div class='colorChart'>
       </div>
 
-      <form>
+      <form @submit="submitYears">
+        <label>Year One</label>
+        <input type="number" v-model='yearOne' name="yearOne" placeholder="Enter First Year">
+        <input type="number" v-model="yearTwo" name="yearTwo" placeholder="Enter Second Year">
+        <div>
+          <button class="btn btn-primary font" type="submit">Submit</button>
+        </div>
       </form>
 
     </section>
@@ -20,6 +26,19 @@
 <script>
 export default {
   name: 'ColorChart',
+  data() {
+    return {
+      yearOne: 0,
+      yearTwo: 0,
+    }
+  },
+  methods: {
+    submitYears(evt){
+      evt.preventDefault();
+      console.log('hi Mike')
+      console.log({yearOne: this.yearOne})
+    },
+  },
 }
 </script>
 
@@ -33,15 +52,17 @@ export default {
   text-align: center;
 }
 
-section {
-}
-
 .colorChart {
   height: 150px;
   margin-left: 25%;
   margin-right: 25%;
   border: 2px solid red;
   background-color: #FFD700;
+}
+
+form {
+  text-align: center;
+  margin-top: 25px;
 }
 
 </style>
