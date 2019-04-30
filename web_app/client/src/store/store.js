8dcb9acccb2b6ccf31b732320835de0192acaa2a
ravenusmc <mcuddy77@gmail.com>
2019-04-29 20:05:23 -0400

    started working on the store file for vuex

--- web_app/client/src/store/store.js
+++ web_app/client/src/store/store.js
@@ -4,3 +4,34 @@ import axios from 'axios';
 import jsonp from 'jsonp';
 
 Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+
+  state: {
+    graphOneData: [],
+  },
+
+  getters: {
+    graphOneData: state => state.graphOneData,
+  },
+
+  actions: {
+      fetchGraphOneData: ({ commit }, payload) => {
+        const path = 'http://localhost:5000/incidents_per_year';
+        axios.post(path, payload)
+        .then((res) => {
+          commit('setGraphOneData', res.data)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      },
+    },
+
+  mutations: {
+    setGraphOneData(state, data) {
+      state.graphOneData = data;
+    },
+  },
+
+})
