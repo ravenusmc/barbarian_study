a5f8ad2ee3dce985dea9290f3de4e6bd9d330287
ravenusmc <mcuddy77@gmail.com>
2019-05-02 20:31:17 -0400

    minor changes

--- web_app/server/server.py
+++ web_app/server/server.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+#importing code that I wrote
+from data import *
+
 # configuration
 DEBUG = True
 
@@ -15,6 +18,7 @@ CORS(app)
 @app.route('/colorChart', methods=['GET', 'POST'])
 def colorChart():
     if request.method == 'POST':
+        data = Data()
         post_data = request.get_json()
         yearOne = int(post_data['yearOne'])
         yearTwo = int(post_data['yearTwo'])
