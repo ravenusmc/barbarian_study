2b851c7c13956e2df0b2a1d69db39e3c7c546a11
ravenusmc <mcuddy77@gmail.com>
2019-05-19 11:41:27 -0400

    started work on table for chart

--- web_app/client/src/components/data/GraphOne.vue
+++ web_app/client/src/components/data/GraphOne.vue
@@ -2,14 +2,105 @@
   <div>
 
     <div>
-      <h2 class='font center'>Looking at Roman Victories by  Intervals</h2>
+      <h2 class='font center'>Looking at Roman Victories by Intervals</h2>
     </div>
 
-    <div>
-     <v-chart :chartData="adjustData"></v-chart>
-     <div>
-     </div>
-    </div>
+    <section id='chartSection'>
+
+      <div>
+        <v-chart :chartData="buildGraph"></v-chart>
+      <div>
+
+      </div>
+        <table>
+          <tr>
+            <th>Interval</th>
+            <th>Years</th>
+          </tr>
+          <tr>
+            <td>
+              1
+            </td>
+            <td>
+              -113 B.C.E to -38 B.C.E
+            </td>
+          </tr>
+          <tr>
+            <td>
+              2
+            </td>
+            <td>
+              -38 B.C.E to 37 C.E
+            </td>
+          </tr>
+          <tr>
+            <td>
+              3
+            </td>
+            <td>
+              37 C.E to 112 C.E
+            </td>
+          </tr>
+          <tr>
+            <td>
+              4
+            </td>
+            <td>
+              112 C.E to 187 C.E
+            </td>
+          </tr>
+          <tr>
+            <td>
+              5
+            </td>
+            <td>
+              187 C.E to 262 C.E
+            </td>
+          </tr>
+          <tr>
+            <td>
+              6
+            </td>
+            <td>
+              262 C.E to 337 C.E
+            </td>
+          </tr>
+          <tr>
+            <td>
+              7
+            </td>
+            <td>
+              337 C.E to 412 C.E
+            </td>
+          </tr>
+          <tr>
+            <td>
+              8
+            </td>
+            <td>
+              412 C.E to 487 C.E
+            </td>
+          </tr>
+          <tr>
+            <td>
+              9
+            </td>
+            <td>
+              487 C.E to 562 C.E
+            </td>
+          </tr>
+          <tr>
+            <td>
+              10
+            </td>
+            <td>
+              563 C.E to 637 C.E
+            </td>
+          </tr>
+        </table>
+      </div>
+
+    </section>
 
   </div>
 </template>
@@ -26,7 +117,6 @@ export default {
     return {
       height: 600,
       width: 600,
-      xAxisLabels: ['1','2','3','4','5','6','7','8','9','10'],
       vBarChartData: [],
     }
   },
@@ -39,7 +129,7 @@ export default {
     ...mapGetters([
       'graphOneData',
     ]),
-    adjustData() {
+    buildGraph() {
       return this.vBarChartData = {
         chartType: "vBarChart",
         label: true,
@@ -53,9 +143,9 @@ export default {
         dim: "Interval",
         data: this.graphOneData,
         grid: {
-  enabled: true,
-  gridTicks: 25,
-},
+          enabled: true,
+          gridTicks: 25,
+        },
         overrides: {
           palette: {
             fill: ['#A91DEB', '#4fc08d'],
@@ -90,5 +180,30 @@ export default {
 
 .center {
   text-align: center;
+}
+
+#chartSection {
+  border: 2px solid red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+table {
+  font-family: 'Cinzel', serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+  text-align: center;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
 }
 </style>
