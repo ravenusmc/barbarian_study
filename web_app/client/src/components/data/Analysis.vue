8df9e089b480e0178dd86322b458e61503796198
ravenusmc <mcuddy77@gmail.com>
2019-05-22 19:55:54 -0400

    added in brief conclusion

--- web_app/client/src/components/data/Analysis.vue
+++ web_app/client/src/components/data/Analysis.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-    <h1>Hi</h1>
+
+    <hr>
+
+    <section class='center'>
+      <h2 class='font'>Brief Conclusion</h2>
+      <p class='font'>
+          The first major encounter with Germanic tribes was in -113 B.C.E with the
+        Cimbri. The Romans were slowly expanding North and I believe that tribe
+        was moving south. They were bound to collide. From there, the Romans really
+        did not have that many engagements until about the middle of the third
+        century. This could be cause the Roman empire, at this time, was in a
+        state of crisis - a period known as the crisis of the third century. It
+        almost seems that from this point forward it was nothing but Barbarians
+        at the gate. The Romans have a lot of battles and at the same time having a
+        lot of internal political problems.
+          I also want to point out that it appears that from any given time period
+        the Romans were only winning battles about 50% of the time. This is quite
+        interesting to me. For I've always heard that Rome was a military machine
+        but still it was only winning about 50% of the time against these tribes!
+        That should tell anyone how much of a threat they were! 
+      </p>
+    </section>
+
   </div>
 </template>
 
@@ -11,4 +33,17 @@ export default {
 </script>
 
 <style scoped>
+.font {
+  font-family: 'Cinzel', serif;
+  margin-top: 50px;
+}
+
+.center {
+  text-align: center;
+}
+
+section {
+  margin-left: 25%;
+  margin-right: 25%;
+}
 </style>
